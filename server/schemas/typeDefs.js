d43c20fbe6e87b515b9a3567897f30d6e1de1517
jess-smith49 <js.jessicasmith774@gmail.com>
2021-02-15 13:41:38 -0600

    resolvers and typedef for user login and signup

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,23 @@
+const {gql} = require('apollo-server-express');
+const { typeDefs } = require('.');
+
+typeDefs = gql `
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+    }
+
+    type Query: {
+        me: User
+        users: [User]
+    }
+
+    type Mutation {
+        login(username: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+    }
+    
+    `
+module.exports = typeDefs;
+    
