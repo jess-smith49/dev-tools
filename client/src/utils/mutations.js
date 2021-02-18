285191e029811865a02fdd017e12c7e8a12bfbb3
Maya Santiago <msatori.code@gmail.com>
2021-02-17 20:13:08 -0600

    why

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,58 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql `
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`
+
+export const ADD_USER = gql `
+    mutation addUser($username: String!, $email: String!, $password: String!){
+        addUser(username: $username, email: $email, $password: password){
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`
+
+export const ADD_SET = gql `
+    mutation addSet($setName: String!){
+        addSet(setName: $setName){
+            RETURN SET
+        }
+    }
+`
+
+export const ADD_CARD = gql `
+    mutation addCard($question: String!, $answer: String!){
+        addCard(question: $question, answer: $answer){
+            RETURN SET
+        }
+    }
+    `
+
+export const REMOVE_SET = gql `
+    mutation removeSet($setName: String!){
+        removeSet(setName: $setName){
+            RETURN SET
+        }
+    }
+`
+
+export const REMOVE_CARD = gql `
+    mutation removeCard($question: String!, $answer: String!){
+        removeCard(question: $question, answer: $answer){
+            RETURN SET
+        }
+    }
+`
+
