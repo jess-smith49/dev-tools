0fc3f0af9d53981dc10099ebd607c699d9d99305
jess-smith49 <js.jessicasmith774@gmail.com>
2021-02-20 08:16:58 -0600

    fixed backend/new errors now

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,40 +1,45 @@
 const {gql} = require('apollo-server-express');
-const { typeDefs } = require('.');
 
-typeDefs = gql `
-    type User {
-        _id: ID!
-        username: String!
-        email: String!
-        sets: [Sets]
-    }
+const typeDefs = gql `
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    sets: [Sets]
+}
 
-    type Set {
-        _id: ID!
-        setName: String!
-        card: [Card]
-    }
+type Sets {
+    _id: ID!
+    setName: String!
+    card: [Card]
+}
 
-    type Card {
-        _id: ID!
-        question: String!
-        answer: String!
-    }
+type Card {
+    _id: ID!
+    question: String!
+    answer: String!
+}
 
-    type Query: {
-        me: User
-    }
+type Auth {
+    token: ID!
+    user: User
+}
 
-    type Mutation {
-        login(username: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        addSet(setName: String!): Set
-        addCard(question: String!, answer: String!): Set
-        removeSet(setId: ID!): Set
-        removeCard(cardId: ID!): Set
-    }
-    
-    `
+type Query {
+    me: User
+}
+
+type Mutation {
+    login(username: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addSet(setName: String!): Sets
+    addCard(question: String!, answer: String!): Sets
+    removeSet(setId: ID!): Sets
+    removeCard(cardId: ID!): Sets
+}
+
+`
+
 module.exports = typeDefs;
 
 
