c3d3ec51a2b31acb1b2d89670fa922434ae42374
Maya Santiago <msatori.code@gmail.com>
2021-02-21 16:06:11 -0600

    added header to dash

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import Header from '../../components/Header'
 
 export default function Dashboard() {
-    return(
-        <section className="dash-container container-fluid">
-            <div className="dash-cards card-deck">
-                <div className="card">
-                    <div className="card-body">
-                        Category Goes Here
+    return (
+        <section className="dash">
+            <Header />
+            <div className="dash-container container-fluid">
+                <div className="dash-cards card-deck">
+                    <div className="card">
+                        <div className="card-body">
+                            Category Goes Here
+                    </div>
                     </div>
                 </div>
             </div>
