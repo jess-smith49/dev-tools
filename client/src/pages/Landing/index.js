58552397b43d31d300b60f353107160b7966eecf
Maya Santiago <msatori.code@gmail.com>
2021-02-23 11:45:59 -0600

    moved carosuel to it's own component

--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -1,32 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Carosuel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import SetCarosuel from '../../components/Carosuel';
 
 export default function CTA() {
-    //create breakpoints for carosuel
-    const responsive = {
-        superLargeDesktop: {
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
+    
+  
 
     return (
-        <div className="apply-grad">
-            <section className="cta">
+        <section className="landing-container">
+            <div className="cta">
                 <div className="cta-title">
                     <h1 className="h-lg">Dev Learn</h1>
                 </div>
@@ -43,18 +25,12 @@ export default function CTA() {
                         <Link to="/login">Login </Link>
                     </div>
                 </div>
-                <section className="carosuel">
-                    <div className="carosuel-container">
-                        <Carosuel responsive={responsive}>
-                            <div>Item 1</div>
-                            <div>Item 2</div>
-                            <div>Item 3</div>
-                            <div>Item 4</div>
-                        </Carosuel>
-                    </div>
-                </section>
-
+                </div>
+           
+                <div className="carosuel">
+                    <SetCarosuel />
+                </div>
             </section>
-        </div>
+
     )
 };
