b8f5e1075abbd5f0bf9567dcf5e57952dd6af35f
jess-smith49 <js.jessicasmith774@gmail.com>
2021-02-27 15:43:15 -0600

    stuff

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import {Link, Router} from 'react-router-dom';
 import Header from '../../components/Header';
-import { Card, Button, Modal, CardDeck } from 'react-bootstrap';
+import { Card, Button, Modal, CardDeck, Form } from 'react-bootstrap';
 import CreateSet from '../../components/AddSet';
+import {ADD_SET} from '../../utils/mutations';
+import { useMutation, useQuery } from '@apollo/react-hooks';
 
 
 
@@ -13,12 +15,47 @@ export default function Dashboard() {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     // render seeded cards
+
+//     //query stuff
+//     const {data} = useQuery(QUERY_SET);
+//     if(data){
+//         userSets = data.userSets;
+//     };
+
+     //mutation stuff
+     const [setName, setText] = useState('');
+     const [addSet, {error}] = useMutation(ADD_SET);
+ 
+     const handleChange = e => {
+         const {name, value} = e.target;
+         console.log(setName);
+         setText(e.target.value)
+     
+ };
+     const handleFormSubmit = async event => {
+         event.preventDefault();
+         console.log(setName)
+         
+         try{
+              await addSet({
+                 variables: {setName}
+             });
+             console.log(setName)
+             
+             setText('')
+         }
+         catch(e) {
+             console.log(error)
+         }
+ 
+     }
     return (
         <div>
             <Header />
         <section className="dash">
 
             <CardDeck className="dash-wrap">
+                {/* mapping over cards */}
             <Card md={2}>
                 <Card.Body>
                     <Link to='/flashcards'>Set Name Here</Link>
@@ -31,9 +68,20 @@ export default function Dashboard() {
                 </Button>
                 <Modal show={show} onHide={handleClose}>
                     <Modal.Header closeButton>
-                   <Modal.Title>Modal heading</Modal.Title>
+                   <Modal.Title>Enter in a Name for your Set</Modal.Title>
                  </Modal.Header>
-               <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+               <Modal.Body>
+               <Form onSubmit={handleFormSubmit}>
+                       <h1>Name:</h1>
+                       <textarea
+                        
+                        name="set"
+                        value={setName}
+                        onChange={handleChange}
+                       >
+                       </textarea>
+                   </Form>
+               </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={handleClose}>
                             Close
--- client/src/pages/SetWrapper/index.js
+++ client/src/pages/SetWrapper/index.js
@@ -1,7 +1,10 @@
+import { useQuery } from '@apollo/react-hooks';
 import { React, useState } from 'react';
 import { CardGroup, Card, Modal, Button, CardDeck } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 import FlashCard from '../../components/FlashCard';
 import Header from '../../components/Header';
+import {QUERY_SET} from '../../utils/queries';
 
 
 
@@ -11,11 +14,18 @@ export default function CardSet() {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    const {id: setId} = useParams();
+    const {loading, data} = useQuery(QUERY_SET, {
+        variables: {id: setId}
+    })
+
+    const set = data?.set || {};
+
     return (
         <section className="set-wrap">
             <Header />
             <div className="set-body">
-                <h1>HTML</h1>
+                <h1>{set.setName}</h1>
                 <div className="set-modal"></div>
                 <Button variant="primary" onClick={handleShow}>
                     Add A Card To Your Set
